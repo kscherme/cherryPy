84707d243b8e25f22173167fb06e5e77b4a3cc6d
Katie Schermerhorn <katieschermerhorn@katies-mbp.dhcp.nd.edu>
2017-03-22 12:06:39 -0400

    working

--- reset.py
+++ reset.py
@@ -13,9 +13,9 @@ class ResetController(object, mdb = None):
 			mdb.load_movies('ml-1m/movies.dat')
 			mdb.load_users('ml-1m/users.dat')
 			mdb.load_ratings('ml-1m/ratings.dat')
-       	except KeyError as ex:
-       		output['result'] = 'error'
+		except KeyError as ex:
+			output['result'] = 'error'
 
-       	return json.dumps(output)
+		return json.dumps(output)
 
 	
