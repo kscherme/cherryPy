9d467269c50ffd1b000422c158f72a44748234d0
Katie Schermerhorn <katieschermerhorn@katies-mbp.dhcp.nd.edu>
2017-03-23 23:47:51 -0400

    i

--- reset.py
+++ reset.py
@@ -4,15 +4,19 @@
 import cherrypy
 import re,json
 
-class ResetController(object, mdb = None):
+class ResetController(object):
 
-	def PUT():
+	def __init__(self, mdb = None):
+		self.mdb = mdb
+
+
+	def PUT(self):
 
 		output = {'result':'success'}
 		try:
-			mdb.load_movies('ml-1m/movies.dat')
-			mdb.load_users('ml-1m/users.dat')
-			mdb.load_ratings('ml-1m/ratings.dat')
+			self.mdb.load_movies('ml-1m/movies.dat')
+			self.mdb.load_users('ml-1m/users.dat')
+			self.mdb.load_ratings('ml-1m/ratings.dat')
 		except KeyError as ex:
 			output['result'] = 'error'
 
