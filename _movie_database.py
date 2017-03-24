ae7c24a3ef30085a58fc03da3f75791f96f9bb0f
kscherme <kscherme@nd.edu>
2017-03-24 11:55:51 -0400

    movies

--- _movie_database.py
+++ _movie_database.py
@@ -111,6 +111,6 @@ if __name__ == "__main__":
        #mdb.print_sorted_movies()
        mdb.load_ratings('ml-1m/ratings.dat')
        print (mdb.get_rating(1))
-       print (mdb.get_movie(1))
+       print (mdb.get_movies())
        
        #print mdb.get_rating(787)
--- movies.py
+++ movies.py
@@ -10,18 +10,18 @@ class MovieController(object):
 		self.mdb = mdb
 
 	def GET_INDEX(self):
-		output = {'result':'success'}
-		try:
-			output['movies'] = []
-			movies = mdb.get_movies()
-			for key,value in movies.items():
-
-				smallDict = {'id':key, 'genres':value[1], 'title':value[0], 'result':'success'}
-				output['movies'].append(smallDict)
-		except KeyError as ex:
-			output['result'] = 'error'
-
-		return json.dumps(output)
+                output = {'result':'success'}
+                try:
+                        output['movies'] = []
+                        movie_ids = self.mdb.get_movies()
+                        for mid in movie_ids:
+                            movie_info = self.mdb.get_movie(mid)
+                            smallDict = {'id':mid, 'genres':movie_info[1], 'title':movie_info[0], 'result':'success'}
+                            output['movies'].append(smallDict)
+                except KeyError as ex:
+                        output['result'] = 'error'
+                        
+                return json.dumps(output)
 
 
 	
