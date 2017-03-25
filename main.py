89b642d269170fe0e3880da08953e9435d2d0ffb
Katie Schermerhorn <katieschermerhorn@katies-mbp.dhcp.nd.edu>
2017-03-25 15:13:13 -0400

    users

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from _movie_database import _movie_database
 import cherrypy
 from reset import ResetController
 from movies import MovieController
+from users import UserController
 
 def start_service(mdb):
 
 	# create resource objects
 	resetController = ResetController(mdb)
 	movieController = MovieController(mdb)
+	userController = UserController(mdb)
 
 	dispatcher = cherrypy.dispatch.RoutesDispatcher()
 
@@ -26,6 +28,14 @@ def start_service(mdb):
 	dispatcher.connect('movie_put', '/movies/:mid', controller=movieController, action = 'PUT', conditions=dict(method=['PUT']))
 	dispatcher.connect('movie_delete_index', '/movies/', controller=movieController, action = 'DELETE_INDEX', conditions=dict(method=['DELETE']))
 	dispatcher.connect('movie_delete', '/movies/:mid', controller=movieController, action = 'DELETE', conditions=dict(method=['DELETE']))
+
+	# user connections
+	dispatcher.connect('user_get_index', '/movies/', controller=userController, action = 'GET_INDEX', conditions=dict(method=['GET']))
+	dispatcher.connect('user_get', '/movies/:uid', controller=userController, action = 'GET', conditions=dict(method=['GET']))
+	dispatcher.connect('user_post_index', '/movies/', controller=userController, action = 'POST', conditions=dict(method=['POST']))
+	dispatcher.connect('user_put', '/movies/:uid', controller=userController, action = 'PUT', conditions=dict(method=['PUT']))
+	dispatcher.connect('user_delete_index', '/movies/', controller=userController, action = 'DELETE_INDEX', conditions=dict(method=['DELETE']))
+	dispatcher.connect('user_delete', '/movies/:uid', controller=userController, action = 'DELETE', conditions=dict(method=['DELETE']))
 
 	conf = { 'global' 	: {'server.socket_host': 'ash.campus.nd.edu',
 							'server.socket_port': 40100,},
