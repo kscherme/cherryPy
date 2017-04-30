077e7c6987efdba48c347f909da1d7630294d6f5
Katie Schermerhorn <katieschermerhorn@katies-mbp.dhcp.nd.edu>
2017-04-30 16:55:39 -0400

    help

--- twisted/work.py
+++ twisted/work.py
@@ -1,14 +1,20 @@
 from twisted.internet.protocol import ClientFactory
 from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
+from twisted.internet.defer import DeferredQueue
 
 class MyServiceConnection(Protocol):
 
+	def __init__(self, data_conn):
+		self.data_conn = data_conn
+
 	def connectionMade(self):
 		print "Service Connection Made!"
+		self.data_conn.startForwarding(self)
 
 	def dataReceived(self, data):
 		print "Got data over service connection: ", data
+		self.data_conn.transport.write(data)
 
 
 class MyCommandConnection(Protocol):
@@ -25,18 +31,32 @@ class MyCommandConnection(Protocol):
 
 class MyDataConnection(Protocol):
 
+	def __init__(self):
+		self.queue = DeferredQueue()
+		self.service_conn = None
+
 	def connectionMade(self):
 		print "Data Connection Made!"
 		# Create service connection
-		reactor.connectTCP("student00.cse.nd.edu", 22, MyServiceConnectionFactory())
+		reactor.connectTCP("student00.cse.nd.edu", 22, MyServiceConnectionFactory(self))
 
 	def dataReceived(self, data):
 		print "Got data over data connection: ", data
+		self.queue.put(data)
+
+	def startForwarding(self, service_conn):
+		self.service_conn = service_conn
+		self.queue.get().addCallback(self.forwardData)
+
+	def forwardData(self, data):
+		self.service_conn.transport.write(data)
+		self.queue.get().addCallback(self.forwardData)
+
 
 class MyServiceConnectionFactory(ClientFactory):
 
-	def __init__(self):
-		self.myservconn = MyServiceConnection()
+	def __init__(self, data_conn):
+		self.myservconn = MyServiceConnection(data_conn)
 
 	def buildProtocol(self, addr):
 		return self.myservconn
